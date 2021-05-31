f087b22af3f94da326f13d7321d3480bdead9b98
Sedat BAŞ <sedatbass.1992@gmail.com>
2021-05-31 22:14:02 +0300

    NextJS

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import "../assets/styles/app.css"
+import "../assets/styles/navigation.css"
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Layout from '../components/layout'
+
+
+function HomePage() {
+    return(
+        <Layout>
+            <Head>
+                <title>Ana sayfa</title>
+            </Head>
+            <h1>Merhaba</h1>
+        </Layout>
+    ) 
+  }
+  
+  export default HomePage
